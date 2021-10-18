271cb7e27a979e6cc243090a8426bb9c66fb0b62
Takumi Futamata <92451277+TakumiJapan@users.noreply.github.com>
2021-10-18 12:03:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import selenium.webdriver as webdriver
 import time
 from email.message import EmailMessage
 import smtplib
-from conf import query, num_page, receiver, e_login, e_password
+from conf import query, num_page, sender, password, receiver
 
-query_link = f"https://www.semanticscholar.org/search?q={query}&page="
+query_link = f"https://www.semanticscholar.org/search?q={query}&sort=relevance&page="
 
 # working paths
 working_dir = os.path.dirname(os.path.realpath(__file__))
@@ -26,66 +26,57 @@ prefs = {"download.default_directory": folder_for_pdf, "download.prompt_for_down
 chrome_options.add_experimental_option('prefs', prefs)
 os.environ["webdriver.chrome.driver"] = webdriver_path   # 'webdriver' executable needs to be in PATH. Please see https://sites.google.com/a/chromium.org/chromedriver/home
 
-links_list = [query_link + str(page+1) for page in range(num_page)]  # create links to follow
+links_list = [query_link + str(page+1) for page in range(num_page)]   # create links to follow
 
-driver = webdriver.Chrome(executable_path=webdriver_path, options=chrome_options)
+driver = webdriver.Chrome(executable_path=webdriver_path, chrome_options=chrome_options)
 
-final_info = []   # empty dictionary for articles info
+final_info = []  # empty dictionary for articles info
 
 for search_link in links_list:
-    # get all links to articles from the page
-    iterator = 0
     driver.get(search_link)
     time.sleep(5)
-    articles = driver.find_elements_by_xpath("//*[@data-selenium-selector='title-link']")
-    articles_links = []
-    articles_dates = []
-    temp_dates = driver.find_elements_by_class_name('cl-paper-pubdates')
-    for data in temp_dates:
-        articles_dates.append(data.text)
+    articles = driver.find_elements_by_xpath("//a[@data-selenium-selector='title-link']")
 
+    articles_links = []
     for article in articles:
         try:
             link = article.get_attribute("href")
             articles_links.append(link)
-
         except:
             pass
 
     for link in articles_links:
-        # get info of each article 
-        tmp_info = {}
-
+        # get info of each article
         driver.get(link)
 
-        title = driver.find_elements_by_xpath("//*[@data-selenium-selector='paper-detail-title']")[0].text
-        author = driver.find_elements_by_class_name('paper-meta-item')[0].text
-        tmp_info.update({
-                        'title': title,
-                        'date' : articles_dates[iterator],   # TODO: might convert to datetime
-                        'authors': author
-                        })
+        try: title = driver.find_element_by_xpath("//h1[@data-selenium-selector='paper-detail-title']").text
+        except: title = 'No Title'
 
-        # trying to download the article's doc
+        try: authors = driver.find_element_by_xpath("//span[@class='author-list']").text
+        except: authors = 'No Author'
+
+        try: desc = driver.find_element_by_xpath("//span[@data-selenium-selector='text-truncator-text']").text
+        except: desc = 'No Description'
 
         try:
             initial_dir = os.listdir(folder_for_pdf)
             driver.find_element_by_xpath("//*[@class='alternate-sources__dropdown-wrapper']").click()
             time.sleep(5)
-
             current_dir = os.listdir(folder_for_pdf)
             filename = list(set(current_dir) - set(initial_dir))[0]
             full_path = os.path.join(folder_for_pdf, filename)
-
         except Exception as e:
             full_path = None
 
-        tmp_info.update({'path_to_file':full_path})
+        tmp_info = {}
+        tmp_info.update({
+            'Title': title,
+            'Source': link,
+            'Authors': authors,
+            'Description': desc,
+        })
 
         final_info.append(tmp_info.copy())
-        time.sleep(2)
-
-        iterator +=1
 
 driver.quit()
 
@@ -95,26 +86,25 @@ excel_path = os.path.join(working_dir, "data.xlsx")
 df.to_excel(excel_path, index=False)
 
 # create email
-login, password = e_login, e_password   # could be setup manually
+
 mail = EmailMessage()
-mail['From'] = login
+mail['From'] = sender
 mail['To'] = receiver
 mail['Subject'] = "Topics analysis"
 mail.set_content("Hi!\n\nFind attached excel file with articles info.\n\nRegard,")
 
-# add attachment
+# add article attachment
 with open(excel_path, 'rb') as f:
     file_data = f.read()
-    file_name = f'articles_info.xlsx'
+    file_name = f'data.xlsx'
 mail.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
 
 # send email
-server = smtplib.SMTP('smtp.office365.com')  
-server.starttls()  
-server.login(login, password)    
-server.send_message(mail)      
-server.quit()      
-
+server = smtplib.SMTP('smtp.office365.com')
+server.starttls()
+server.login(sender, password)
+server.send_message(mail)
+server.quit()
 
     # ┏━┓┏━┓┏━━━┓┏━━━━┓┏━━━┓┏━━━┓┏━━━┓  
     # ┃ ┗┛ ┃┃┏━┓┃┗━━┓ ┃┃┏━┓┃┃┏━┓┃┃┏━┓┃  
